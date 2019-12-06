8701312c6b4f2a5462ce96c9951c30863f2a546e
Michał Warda <michal.warda@elpassion.pl>
2019-12-06 14:47:32 +0100

    Release 0.0.5

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,5 +2,6 @@ import { LaunchOptions } from "puppeteer";
 export interface IScreenshotOptions {
     launchOptions: LaunchOptions;
 }
+export declare const screenshot: ({ launchOptions }?: IScreenshotOptions) => Promise<void>;
 export declare const takeScreenshotOnTestError: (options?: IScreenshotOptions) => void;
 //# sourceMappingURL=index.d.ts.map
