6dfb5ffebf2dc3081b81b87d7ec2b2043f23fc70
Michał Warda <michal.warda@elpassion.pl>
2019-12-06 14:18:10 +0100

    Release 0.0.2

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import pupetter from "puppeteer";
+export declare const takeScreenshotOnError: (params: [({
+    launchOptions?: pupetter.LaunchOptions | undefined;
+} | undefined)?]) => void;
+//# sourceMappingURL=index.d.ts.map
